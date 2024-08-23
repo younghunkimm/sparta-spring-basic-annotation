65a03c520c63c9530e8eed28730b3391c5e7a29b
UK <hwangwonuk94@gmail.com>
2024-08-23 13:51:28 +0900

    feat: ParameterController headers

--- src/main/java/com/example/springbasicannotation/controller/ParameterController.java
+++ src/main/java/com/example/springbasicannotation/controller/ParameterController.java
@@ -1,17 +1,34 @@
 package com.example.springbasicannotation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ParameterController {
 
     // parms 속성값 추가
-    @GetMapping(value = "/users", params = "gender=man")
-    public String params() {
+//    @GetMapping(value = "/users", params = "gender=man")
+//    public String params() {
+//        // logic
+//        String result = "params API가 호출 되었습니다.";
+//        return result;
+//    }
+
+    // headers 속성값 추가
+    @PostMapping(value = "/users", headers = "Content-Type=application/json")
+    public String headers() {
         // logic
-        String result = "params API가 호출 되었습니다.";
+        String result = "headers API가 호출 되었습니다.";
         return result;
     }
+
+    // headers 속성값 추가
+//    @PostMapping(value = "/users", consumes = "application/json") // MediaType.APPLICATION_JSON_VALUE
+//    public String consumes() {
+//        // logic
+//        String result = "consumes API가 호출 되었습니다.";
+//        return result;
+//    }
 
 }
