b36f89a24fd2a2fe1ec93c7e7114bf95009e2b47
UK <hwangwonuk94@gmail.com>
2024-08-23 13:22:25 +0900

    feat: PathVariableController v4

--- src/main/java/com/example/springbasicannotation/controller/PathVariableController.java
+++ src/main/java/com/example/springbasicannotation/controller/PathVariableController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RequestMapping("/posts")
+ @RequestMapping("/posts/{postId}")
+//@RequestMapping("/posts")
 @RestController
 public class PathVariableController {
     // postId로 된 post 단건 조회
-    @GetMapping("/{postId}")
-    public String pathVariableV1(@PathVariable("postId") Long data) {
-        // logic
-        String result = "PathvariableV1 결과입니다 : " + data;
-        return result;
-    }
+//    @GetMapping("/{postId}")
+//    public String pathVariableV1(@PathVariable("postId") Long data) {
+//        // logic
+//        String result = "PathvariableV1 결과입니다 : " + data;
+//        return result;
+//    }
 
     // 변수명과 같다면 속성값 생략가능
 //    @GetMapping("/{postId}")
@@ -25,13 +25,22 @@ public class PathVariableController {
 //        return result;
 //    }
 
-    @GetMapping("/{postId}/comments/{commentId}")
-    public String pathVariableV3(
+//    @GetMapping("/{postId}/comments/{commentId}")
+//    public String pathVariableV3(
+//            @PathVariable Long postId,
+//            @PathVariable Long commentId
+//    ) {
+//        // logic
+//        String result = "PathvariableV3 결과입니다 postId : " + postId + "commentsId : " + commentId;
+//        return result;
+//    }
+    @GetMapping("/comments/{commentId}")
+    public String pathVariableV4(
             @PathVariable Long postId,
             @PathVariable Long commentId
     ) {
         // logic
-        String result = "PathvariableV3 결과입니다 postId : " + postId + "commentsId : " + commentId;
+        String result = "PathvariableV4 결과입니다 postId : " + postId + "commentsId : " + commentId;
         return result;
     }
 
