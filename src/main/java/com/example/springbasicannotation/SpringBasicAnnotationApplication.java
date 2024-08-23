1d1949410d4c8b4973c037cf7bfbcb52facabc3c
UK <hwangwonuk94@gmail.com>
2024-08-23 09:48:29 +0900

    init

---
+++ src/main/java/com/example/springbasicannotation/SpringBasicAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicAnnotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasicannotation/SpringBasicAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
