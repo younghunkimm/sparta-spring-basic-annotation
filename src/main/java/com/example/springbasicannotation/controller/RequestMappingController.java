55fe67dab06f5f9fb1d3eb0528d3a053c3d51236
UK <hwangwonuk94@gmail.com>
2024-08-23 11:45:06 +0900

    feat: RequestMappingController v2

--- src/main/java/com/example/springbasicannotation/controller/RequestMappingController.java
+++ src/main/java/com/example/springbasicannotation/controller/RequestMappingController.java
@@ -1,8 +1,6 @@
 package com.example.springbasicannotation.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // 응답 데이터를 반환한다.
 @RestController
@@ -14,5 +12,36 @@ public class RequestMappingController {
         // logic
         return "this is sparta!";
     }
+
+    // Post, GET, Put, Patch, Delete 모두 가능
+    @GetMapping(value = "/v2")
+    public String exampleV2() {
+        // logic
+        return "this is sparta!";
+    }
+
+//    @PostMapping(value = "/v2")
+//    public String exampleV2() {
+//        // logic
+//        return "this is sparta!";
+//    }
+//
+//    @PutMapping(value = "/v2")
+//    public String exampleV2() {
+//        // logic
+//        return "this is sparta!";
+//    }
+//
+//    @PatchMapping(value = "/v2")
+//    public String exampleV2() {
+//        // logic
+//        return "this is sparta!";
+//    }
+//
+//    @DeleteMapping(value = "/v2")
+//    public String exampleV2() {
+//        // logic
+//        return "this is sparta!";
+//    }
 
 }
