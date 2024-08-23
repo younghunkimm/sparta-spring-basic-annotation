4b8741410eec1fddf77e239faf89ff290f5ab1c9
UK <hwangwonuk94@gmail.com>
2024-08-23 13:19:09 +0900

    feat: PathVariableController v3

--- src/main/java/com/example/springbasicannotation/controller/PathVariableController.java
+++ src/main/java/com/example/springbasicannotation/controller/PathVariableController.java
@@ -5,22 +5,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RequestMapping("/posts")
 @RestController
 public class PathVariableController {
     // postId로 된 post 단건 조회
+    @GetMapping("/{postId}")
+    public String pathVariableV1(@PathVariable("postId") Long data) {
+        // logic
+        String result = "PathvariableV1 결과입니다 : " + data;
+        return result;
+    }
+
+    // 변수명과 같다면 속성값 생략가능
 //    @GetMapping("/{postId}")
-//    public String pathVariableV1(@PathVariable("postId") Long data) {
+//    public String pathVariableV2(@PathVariable Long postId) {
 //        // logic
-//        String result = "PathvariableV1 결과입니다 : " + data;
+//        String result = "PathvariableV2 결과입니다 : " + postId;
 //        return result;
 //    }
 
-    // 변수명과 같다면 속성값 생략가능
-    @GetMapping("/{postId}")
-    public String pathVariableV2(@PathVariable Long postId) {
+    @GetMapping("/{postId}/comments/{commentId}")
+    public String pathVariableV3(
+            @PathVariable Long postId,
+            @PathVariable Long commentId
+    ) {
         // logic
-        String result = "PathvariableV2 결과입니다 : " + postId;
+        String result = "PathvariableV3 결과입니다 postId : " + postId + "commentsId : " + commentId;
         return result;
     }
 
