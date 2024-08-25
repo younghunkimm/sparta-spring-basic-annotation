d0f883086a24ed5589b0b85d183283937c74c351
UK <hwangwonuk94@gmail.com>
2024-08-25 18:33:43 +0900

    feat: HTTP 요청 데이터 1강

---
+++ src/main/java/com/example/springbasicannotation/controller/ModelAttributeController.java
@@ -0,0 +1,55 @@
+package com.example.springbasicannotation.controller;
+
+import com.example.springbasicannotation.entity.Tutor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ModelAttributeController {
+
+    @ResponseBody
+    @RequestMapping("/v1/tutor")
+    public String requestParamV1(
+            @RequestParam String name,
+            @RequestParam int age
+    ) {
+
+        Tutor tutor = new Tutor();
+        tutor.setName(name);
+        tutor.setAge(age);
+
+        return "tutor name = " + name + " age = " + age;
+    }
+
+    @ResponseBody
+    @RequestMapping("/v2/tutor")
+    public String modelAttributeV2(
+            @ModelAttribute Tutor tutor
+    ) {
+
+        String name = tutor.getName();
+        int age = tutor.getAge();
+
+        return "tutor name = " + name + " age = " + age;
+    }
+
+    @ResponseBody
+    @GetMapping("/v3/tutor")
+    public String modelAttributeV3(Tutor tutor) {
+
+        String name = tutor.getName();
+        int age = tutor.getAge();
+
+        return "tutor name = " + name + " age = " + age;
+    }
+
+    @ResponseBody
+    @GetMapping("/v4/tutor")
+    public String requestParamV2(
+            String name,
+            int age
+    ) {
+        return "tutor name = " + name + " age = " + age;
+    }
+
+}
---
+++ src/main/java/com/example/springbasicannotation/entity/Tutor.java
@@ -0,0 +1,12 @@
+package com.example.springbasicannotation.entity;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class Tutor {
+
+    private String name;
+    private int age;
+
+}
