9e6c0ee9a22aacc4b3e42315fb2e04177ce713ad
UK <hwangwonuk94@gmail.com>
2024-08-25 22:25:21 +0900

    feat: 요청 데이터 2강 Text

---
+++ src/main/java/com/example/springbasicannotation/controller/RequestBodyStringController.java
@@ -0,0 +1,54 @@
+package com.example.springbasicannotation.controller;
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/v1/request-body-text")
+    public void requestBodyTextV1(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+
+        ServletInputStream inputStream = request.getInputStream();
+        String bodyText = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        response.getWriter().write("response = " + bodyText);
+
+    }
+
+    @PostMapping("/v2/request-body-text")
+    public void requestBodyTextV2(
+            InputStream inputStream,
+            Writer responseWriter
+    ) throws IOException {
+
+        String bodyText = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        responseWriter.write("response = " + bodyText);
+
+    }
+
+    @PostMapping("/v3/request-body-text")
+    public HttpEntity<String> requestBodyTextV3(HttpEntity<String> httpEntity) {
+
+        // HttpMessageConverter가 동작해서 아래 코드가 동작하게됨
+        String body = httpEntity.getBody();
+
+        return new HttpEntity<>("response = " + body); // 매개변수 = Body Message
+    }
+
+}
