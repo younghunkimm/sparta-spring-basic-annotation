d339b5ecf7ec016845a9d381e5292d4374ad9b6c
UK <hwangwonuk94@gmail.com>
2024-08-26 00:47:36 +0900

    feat: View Template

---
+++ src/main/java/com/example/springbasicannotation/controller/ViewTemplateController.java
@@ -0,0 +1,30 @@
+package com.example.springbasicannotation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ViewTemplateController {
+
+    @RequestMapping("/response-view")
+    public String responseView(Model model) {
+        model.addAttribute("data", "sparta");
+
+        return "thymeleaf-view";
+    }
+
+    @ResponseBody // @RestController = @Controller + @ResponseBody
+    @RequestMapping("/response-body")
+    public String responseBody() {
+        return "thymeleaf-view";
+    }
+
+    // thymeleaf-view.html 과 Mapping된다.
+    @RequestMapping("/thymeleaf-view")
+    public void responseViewV2(Model model) {
+        model.addAttribute("data", "sparta");
+    }
+
+}
