0aa4671f61a1337e77ec983023bad2f9f281cb38
김영훈 <hunifive@gmail.com>
2025-07-28 17:06:01 +0900

    ModelAttribute v1
    @ModelAttribute 기본 코드

--- src/main/java/com/example/springbasicannotation/controller/ModelAttributeController.java
+++ src/main/java/com/example/springbasicannotation/controller/ModelAttributeController.java
@@ -4,9 +4,35 @@ import com.example.springbasicannotation.entity.Tutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * <h2>{@code @ModelAttribute}</h2>
+ * <p>요청 파라미터를 받아 필요한 {@code Object}로 바인딩 해준다.</p>
+ * <p>주로 {@code HTML} 폼에서 전송된 데이터를 바인딩 하고 {@code HTTP Method POST}인 경우 사용된다.</p>
+ */
 @Controller
 public class ModelAttributeController {
 
+    /**
+     * <h2>기존 코드</h2>
+     *
+     * <ul>
+     *     <li>
+     *         {@code @RequestParam} 의 {@code Mapping} 을 사용하게 되면 아래와 같은 객체를 생성하는 코드가 포함된다.
+     *         <ul>
+     *             <li><b>{@code @ModelAttribute}</b> 는 해당 과정을 자동화 한다.</li>
+     *         </ul>
+     *     </li>
+     *     <li>
+     *         Postman
+     *         <pre>{@code
+     *         POST /v1/tutor
+     *         content-type: application/x-www-form-urlencoded
+     *
+     *         name=wonuk&age=100
+     *         }</pre>
+     *     </li>
+     * </ul>
+     */
     @ResponseBody
     @PostMapping("/v1/tutor")
     public String requestParamV1(
