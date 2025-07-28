d96c3300f0ecc18d9f14efa197dce8e987cd8545
김영훈 <hunifive@gmail.com>
2025-07-28 13:37:52 +0900

    RequestParam-v1

--- src/main/java/com/example/springbasicannotation/controller/RequestParamController.java
+++ src/main/java/com/example/springbasicannotation/controller/RequestParamController.java
@@ -13,6 +13,20 @@ import java.io.IOException;
 @Controller
 public class RequestParamController {
 
+    /**
+     * <h2><b>{@code HttpServletRequest} 사용</b></h2>
+     *
+     * <ul>
+     *     <li>
+     *         {@code response.getWriter().write()}
+     *
+     *         <ul>
+     *             <li>HttpServletResponse를 사용해서 응답값을 직접 다룰 수 있다.</li>
+     *             <li>{@code @Controller} 지만 {@code @ResponseBody} 를 함께 사용한 것과 같다.</li>
+     *         </ul>
+     *     </li>
+     * </ul>
+     */
     @GetMapping("/request-params")
     public void params(
             HttpServletRequest request,
