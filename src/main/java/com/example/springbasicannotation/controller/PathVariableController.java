58e3fca240a29da2b7b4064c2d190a21aa5c512b
김영훈 <hunifive@gmail.com>
2025-07-28 10:29:29 +0900

    PathVariable-GET V2

--- src/main/java/com/example/springbasicannotation/controller/PathVariableController.java
+++ src/main/java/com/example/springbasicannotation/controller/PathVariableController.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PathVariableController {
     // postId로 된 post 단건 조회
-    @GetMapping("/{postId}")
-    public String pathVariableV1(@PathVariable("postId") Long data) {
-        // logic
-        String result = "PathvariableV1 결과입니다 : " + data;
-        return result;
-    }
-
-    // 변수명과 같다면 속성값 생략가능
 //    @GetMapping("/{postId}")
-//    public String pathVariableV2(@PathVariable Long postId) {
+//    public String pathVariableV1(@PathVariable("postId") Long data) {
 //        // logic
-//        String result = "PathvariableV2 결과입니다 : " + postId;
+//        String result = "PathvariableV1 결과입니다 : " + data;
 //        return result;
 //    }
+
+    // 변수명과 같다면 속성값 생략가능
+    @GetMapping("/{postId}")
+    public String pathVariableV2(@PathVariable Long postId) {
+        // logic
+        String result = "PathvariableV2 결과입니다 : " + postId;
+        return result;
+    }
 
 //    @GetMapping("/{postId}/comments/{commentId}")
 //    public String pathVariableV3(
