66378f1e4155b4bfa20f018b2b3ca9f8291b00b5
김영훈 <hunifive@gmail.com>
2025-07-28 17:06:24 +0900

    ModelAttribute v2
    @ModelAttribute 적용

--- src/main/java/com/example/springbasicannotation/controller/ModelAttributeController.java
+++ src/main/java/com/example/springbasicannotation/controller/ModelAttributeController.java
@@ -47,6 +47,65 @@ public class ModelAttributeController {
         return "tutor name = " + name + " age = " + age;
     }
 
+    /**
+     * <h2>{@code @ModelAttribute} 적용</h2>
+     *
+     * <ul>
+     *     <li>
+     *         Postman
+     *         <pre>{@code
+     *         POST /v2/tutor
+     *         content-type: application/x-www-form-urlencoded
+     *
+     *         name=wonuk&age=100
+     *         }</pre>
+     *     </li>
+     *     <li>
+     *         {@code @ModelAttribute} 동작 순서
+     *         <ol>
+     *             <li>파라미터에 {@code @ModelAttribute}가 있으면 파라미터인 {@code Tutor} 객체를 생성한다.</li>
+     *             <li>
+     *                 요청 파라미터 이름으로 객체 필드의 {@code Setter}를 호출해서 바인딩한다.
+     *                 <ol style="list-style-type: lower-alpha">
+     *                     <li>파라미터 이름이 {@code name} 이면 {@code setName(value);} 메서드를 호출한다.</li>
+     *                     <li>파라미터 이름과 필드 이름이 반드시 같아야 한다!</li>
+     *                 </ol>
+     *             </li>
+     *         </ol>
+     *     </li>
+     *     <li>
+     *         {@code Setter}가 없다면?
+     *         <ul>
+     *             <li>
+     *                 <b>객체 필드에 값이 set되지 않는다.</b>
+     *                 <pre>{@code
+     *                 tutor name = null age = 0
+     *                 }</pre>
+     *             </li>
+     *         </ul>
+     *     </li>
+     *     <li>
+     *         파라미터의 타입이 다른 경우
+     *         <ul>
+     *             <li>
+     *                 <p>만약 요청 파라미터 {@code age}에 {@code int}가 아닌 {@code String}이 전달된다면?</p>
+     *                 <p>Ex) {@code http://localhost:8080/v2/tutor} + x-www-form-urlencoded</p>
+     *                 <pre>{@code
+     *                 POST /v2/tutor
+     *                 content-type: application/x-www-form-urlencoded
+     *
+     *                 name=wonuk&age=nbcamp
+     *                 }</pre>
+     *
+     *                 <ul>
+     *                     <li>BindException 발생</li>
+     *                     <li>이런 경우 때문에 <b>Validation(검증)</b>이 필요하다</li>
+     *                 </ul>
+     *             </li>
+     *         </ul>
+     *     </li>
+     * </ul>
+     */
     @ResponseBody
     @PostMapping("/v2/tutor")
     public String modelAttributeV2(
