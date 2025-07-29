b84840d385aa468077736e3a40fc57add5c14139
김영훈 <hunifive@gmail.com>
2025-07-29 12:27:26 +0900

    response-body

--- src/main/java/com/example/springbasicannotation/controller/ViewController.java
+++ src/main/java/com/example/springbasicannotation/controller/ViewController.java
@@ -2,9 +2,16 @@ package com.example.springbasicannotation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ViewController {
+
+    @ResponseBody // @RestController = @Controller + @ResponseBody
+    @RequestMapping("/response-body")
+    public String responseBody() {
+        return "thymeleaf-view";
+    }
 
     @RequestMapping("/view")
     public String example() {
